7122a753c2ecf44930f99d181c56da098f6d3bdd
Tatiana <tavergarap@gmail.com>
2022-08-20 12:57:02 -0500

    Creacion de empresa

---
+++ com/co/UdeACiclo3/Empresa.java
@@ -0,0 +1,8 @@
+package com.co.UdeACiclo3;
+
+public class Empresa {
+
+    public void ImprimirEmpresa (String NombreEmpresa) {
+        System.out.println("vuelve pronto Att: "+ NombreEmpresa);
+    }
+}
