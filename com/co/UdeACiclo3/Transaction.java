778829d987176bbeb79f492a9942cde38dc09538
jesgogu127 <jesgogu@gmail.com>
2022-08-21 22:43:05 -0500

    correción de errores e implementación de los métodos de las clases

---
+++ com/co/UdeACiclo3/Transaction.java
@@ -0,0 +1,7 @@
+package com.co.UdeACiclo3;
+
+public class Transaction{
+    public void ImprimirTransaction (String concepto){
+        System.out.println("El valor de la transaccion fue: " +concepto);
+    }
+}
